3581a7ae208d9604b68d720ed0fe6e20ee2add5d
Bonnegent <richard.bonnegent@gmail.com>
2025-01-14 13:06:14 +0100

    update header & footer, create modals

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import './styles/header.css';
-import './styles/footer.css';
-import './styles/modals/modal_faq.css';
+// import './styles/header.css';
+// import './styles/footer.css';
+// import './styles/modals/modal_faq.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
