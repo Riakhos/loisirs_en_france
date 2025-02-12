2c9133ce7e4d9ed108f2fb9253618da4f090637f
Bonnegent <richard.bonnegent@gmail.com>
2025-02-12 13:29:05 +0100

    Create dashboard user & modal view partner

--- src/Controller/Account/HomeController.php
+++ src/Controller/Account/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Account;
 
+use App\Classe\Cart;
+use App\Repository\RatingRepository;
+use App\Repository\PartnerRepository;
+use App\Repository\ActivityRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,10 +18,27 @@ class HomeController extends AbstractController
      * @return Response
      */
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(
+        // ReservationRepository $reservationRepository, 
+        // MessageRepository $messageRepository, 
+        RatingRepository $ratingRepository,
+        PartnerRepository $partnerRepository,
+        ActivityRepository $activityRepository,
+        Cart $cartService
+    ): Response
     {
+        $user = $this->getUser();
+        
         return $this->render('account/index.html.twig', [
             'controller_name' => 'Tableau de bord',
+            'user' => $user,
+            // 'upcomingReservations' => $reservationRepository->findUpcomingByUser($user),
+            // 'ratings' => $ratingRepository->findBy([
+            //     'user' => $user
+            // ]),
+            'cartItems' => $cartService->getCart(),
+            'partners' => $partnerRepository->findAll(),
+            // 'recentActivities' => $activityRepository->findRecentByUser($user),
         ]);
     }
 }
